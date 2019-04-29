e78cc0cbca67e70399cd956221b71bb45cc9a855
alHasandev <alhasan99dev@gmail.com>
2019-04-29 21:54:17 +0800

    -Menambahkan folder dan file untuk rencana pembuatan templates sederhana -Mengubah file index.html menjadi index.php -Memantapkan struktur web dasar --v0

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- judul singkat dari halaman web -->
+    <title>Repos Dasar</title>
+
+    <!-- disini akan ada link untuk sumber code lain -->
+
+</head>
+
+<body>
+    <h1>Judul Halaman Utama</h1>
+    <!-- akhir dari bagian header -->
+
+    <!-- awal dari bagian navbar -->
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Link 1</a></li>
+        <li><a href="#">Link 2</a></li>
+        <li><a href="#">Link 3</a></li>
+    </ul>
+    <!-- akhir dari bagian navbar -->
+
+    <!-- awal dari bagian content -->
+    <h3>Membuat Struktur dasar untuk CRUD WEB APP Sederhana</h3>
+    <!-- akhir dari bagian content -->
+
+    <!-- awal dari bagian footer -->
+    <div>
+        <p>Copyright &copy; Mohamad Albie 2019</p>
+    </div>
+</body>
+
+</html>
